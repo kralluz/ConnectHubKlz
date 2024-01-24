91570973baccf6b93a9b5eadf563834ecd900fe7
Carlos Henrique <97392902+kralluz@users.noreply.github.com>
2024-01-24 15:02:19 -0300

    feat: configuração do banco de dados com ORM prisma.

--- pages/api/apiTest.js
+++ pages/api/apiTest.js
@@ -1,3 +1,17 @@
-export default function handler(req, res) {
-    res.status(200).json({ teste: "teste de api"});
-}
+const nc = require("next-connect");
+
+const handler = nc()
+    .get((req, res) => {
+        res.status(200).json({ message: "Requisição GET" });
+    })
+    .post((req, res) => {
+        res.status(200).json({ message: "Requisição POST" });
+    })
+    .patch((req, res) => {
+        res.status(200).json({ message: "Requisição PATCH" });
+    })
+    .delete((req, res) => {
+        res.status(200).json({ message: "Requisição DELETE" });
+    });
+
+export default handler;
