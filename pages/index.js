33703e550c6a5a960fb9c9c57a9be24d2e1b391e
Carlos Henrique <97392902+kralluz@users.noreply.github.com>
2024-01-24 18:30:26 -0300

    fix: fetch data error

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,29 @@
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [apiData, setApiData] = useState("");
+    const [apiData, setApiData] = useState("");
 
-  useEffect(() => {
-    fetch('/api/clients')
-    .then(response => response.json())
-    .then(data => setApiData(data.message))
-    .catch(error => console.error('Error fetching data:', error));
-}, []);
-  return (
-    <>
-    <h1>Home</h1>
-    <p>{apiData}</p>
-    </>
-  );
+    useEffect(() => {
+        fetch("/api/clients")
+            .then((response) => response.json())
+            .then((data) => {
+                setApiData(data.message);
+                console.log(data.message);
+            })
+            .catch((error) => console.error("Error fetching data:", error));
+    }, []);
+    return (
+        <>
+            <h1>Home</h1>
+            <ul>
+                {apiData.map((client) => (
+                    <li key={client.id}>
+                        <p>Name: {client.name}</p>
+                        <p>Email: {client.email}</p>
+                        {/* Add more details as needed */}
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 }
