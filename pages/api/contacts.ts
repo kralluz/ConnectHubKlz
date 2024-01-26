ee0b82c93a209863655222ed74c1226ec3de6209
Carlos Henrique <97392902+kralluz@users.noreply.github.com>
2024-01-26 19:45:15 -0300

    Update contacts.ts

--- pages/api/contacts.ts
+++ pages/api/contacts.ts
@@ -1,10 +1,4 @@
 import { createRouter } from "next-connect";
-import { extractId } from "./src/services/session.service";
-import { NextApiRequest, NextApiResponse } from "next";
-import {
-    createContact,
-    readAllContactsByClient,
-} from "./src/services/contact.service";
 import { ContactController } from "./src/controllers/contact.controller";
 
 const router = createRouter();
